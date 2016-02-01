6ea5894288bf928d1a46c2b3dbcede2587344d01
Maritza Ramirez Fernandez <mramirez108@misena.edu.co>
2016-01-31 23:22:47 -0500

    Version 2

--- Ejercicio5/src/App.java
+++ Ejercicio5/src/App.java
@@ -14,6 +14,9 @@ public class App {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Linux");    }
+        System.out.println("Linux Version 2");   
+        
+    }
     
 }
+
